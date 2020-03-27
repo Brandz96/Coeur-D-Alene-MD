959850312c85c90acce953e7d77ff48a5e6ae748
Brandonn <guevb01@pfw.edu>
2020-03-26 20:20:38 -0400

    Beta 1.8
    Re added the custom background with different color for the icon.

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -503,30 +503,31 @@ class BluePainter extends CustomPainter {
 
   Color pickColor(int counter) {
     if (counter % 2 == 0 || counter == 0) {
-      return Colors.white70;
+      return Color.fromRGBO(79, 87, 95, 1);
       //rootDictionaryBackGroundColor = Color.fromRGBO(29, 161, 242, 1);
-      //buttonColors = Color.fromRGBO(187, 134, 252, 1);
+      // ButtonColors = Color.fromRGBO(187, 134, 252, 1);
     } else {
-      return Color.fromRGBO(187, 134, 252, .2);
+      return Color.fromRGBO(128, 128, 128, .3);
     }
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    //final height = size.height;
-    //final width = size.width;
+    final height = size.height;
+    final width = size.width;
     Paint paint = Paint();
 
-//    Path mainBackground = Path();
-//    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    // Path mainBackground = Path();
+//  mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
 //    paint.color = Colors.blue.shade700;
 //    canvas.drawPath(mainBackground, paint);
 
     Path ovalPath = Path();
-    //ovalPath.moveTo(0, height * 0.2);
-    //ovalPath.quadraticBezierTo(width * 0.45, height * 0.25, width * 0.5, height * 0.5);
-    //ovalPath.quadraticBezierTo(width * 0.58, height * 0.8, width * 0.1, height);
-    //ovalPath.lineTo(0, height);
+    ovalPath.moveTo(0, height * 0.2);
+    ovalPath.quadraticBezierTo(
+        width * 0.45, height * 0.25, width * 0.5, height * 0.5);
+    ovalPath.quadraticBezierTo(width * 0.58, height * 0.8, width * 0.1, height);
+    ovalPath.lineTo(0, height);
     paint.color = pickColor(counter);
     //canvas.drawRect(new Rect.fromLTRB(300, 30, 450, 230), paint);
 
